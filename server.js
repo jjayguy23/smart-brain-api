157948c124de4a6c4f6c2a2fc9eddde813889344
Jason Stover <jjayguy23@gmail.com>
2022-03-23 19:14:13 -0400

    initial commit

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/', (req,res)=> { res.send(database.users)})
+app.get("/", (req, res) => {res.send("it's working!");});
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
-})
+app.listen(process.env.PORT || 3000, () => {
+console.log(`app is running on port ${process.env.PORT}`);
+});
